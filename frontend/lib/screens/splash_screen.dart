656e2f36787777dff924c78695c65c16863183a6
Cristina <ab.cristina00@gmail.com>
2025-03-30 14:09:31 +0800

    Update splash_screen.dart

--- frontend/lib/screens/splash_screen.dart
+++ frontend/lib/screens/splash_screen.dart
@@ -15,7 +15,6 @@ class _SplashScreenState extends State<SplashScreen> {
     _navigateToWelcome();
   }
 
-
   _navigateToWelcome() async {
     await Future.delayed(const Duration(seconds: 3));
     if (mounted) {
@@ -25,23 +24,36 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.white,
-    body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SizedBox(
-            width: 80,
-            height: 80,
-            child: Image.asset(
-              'assets/images/LOGO.png', // Path to your Logo image
-              fit: BoxFit.cover, // Adjust the image fit type
-            ),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: RadialGradient(
+            colors: [
+              Color(0xFFF7B733), // Bright Yellow (#fdc830)
+              Color(0xFFFF7904), // Warm Orange (#f37335)
+            ],
+            center: Alignment.center, // Centered gradient
+            radius: 0.8, // Adjust for how far the colors spread
+            focal: Alignment.center, // Focus in the middle
+            focalRadius: 0.2, // Controls color concentration at the center
           ),
-        ],
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 150,
+                height: 150,
+                child: Image.asset(
+                  'assets/images/LOGO.png', // Path to your logo image
+                  fit: BoxFit.cover, // Adjust the image fit type
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
   }
 }
